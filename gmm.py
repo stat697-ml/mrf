New energies, beta sign fix, and update params (still no sigmas)

gmm.py
@@ -2,13 +2,13 @@ import numpy as np
 from random import randint
 import random
 from scipy import stats
-from mrf_3d import Image
+#from mrf_3d import Image
 from scipy import misc
 import math
 import pdb
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-from numba import jit
+#from numba import jit
 sd = 1
 np.random.seed(sd)
 random.seed(sd)
@@ -80,16 +80,17 @@ class GaussianMixtureModel(object):
 	# 			log_arg = 0
 	# 			for k in range(self.K):
 	# 				log_arg+= pi[k](math.log(pi[k]) - math.log
-	# @profile
+	#@profile
 	def estimate_parameters(self, max_iter):
 		i = 0
 		pi_est = self.pi
 		mu_est = self.mu
 		sigma_est = self.sigma
 		while i < max_iter:
+			print(i)
 			# print(pi_est)
-			# print(mu_est)
-			# print(sigma_est)
+			print(mu_est)
+			print(sigma_est)
 			gamma_z = self.E_step(pi_est, mu_est, sigma_est)
 			# pdb.set_trace()
 			[pi_est, mu_est, sigma_est] = self.M_step(gamma_z)
@@ -103,7 +104,7 @@ class GaussianMixtureModel(object):
 if __name__ == '__main__':   
     #### Main Code ####
     NN = 30
-    K = 2
+    K = 4
     # m = [0,0]
     # sigma = [0,0]
     # m[0] = (np.array([4,4]))

mrf_3d.py
@@ -1,12 +1,12 @@
 import numpy as np
 import math, random
 import pdb
-
+from scipy import misc
 from skimage import io, img_as_float, color
 from skimage.exposure import equalize_hist
 from skimage.util import random_noise
 
-# from gmm import GaussianMixtureModel as oliver_gmm
+from gmm import GaussianMixtureModel as oliver_gmm
 
 class Image():
 	"""
@@ -22,12 +22,15 @@ class Image():
 		else:
 			self._data = data
 
+
 		# preprocessing
 		# self._data = color.rgb2lab(self._data)
 		# if self._data.ndim > 2:
 		# 	self._data = equalize_hist(color.rgb2gray(self._data)) # convert to grayscale
 		# self._data = img_as_float(self._data) # lab already normalized??
 		self._data = random_noise(self._data)
+		self._data = self._data[:,:,0:3]
+		#self._data = misc.imresize(self._data,0.5)/255
 
 		(self.height, self.width, self.bitdepth) = self._data.shape
 
@@ -38,7 +41,7 @@ class Image():
 	# piggyback off of numpy's array indexing
 		return self._data.__getitem__(item)
 
-		
+
 class MRF():
 	def __init__(self, image, means, variances):
 		# mean, var comes from gmm
@@ -55,16 +58,16 @@ class MRF():
 
 	### initialization helper functions
 
-	def beta(self, i, j, coeff=-1):
+	def beta(self, i, j, coeff=10):
 		# possible to overwrite this function to modify our energies
 		return coeff
-	
+
 	def estimate_label(self, i, j):
 		# maximum log-likelihood
 		lls = [self.singleton(i,j,k) for k in range(self.no_classes)]
 			   # returns index of minimum as apposed to python's min which would return minimum value
 			   # we want minimum since singleton is negative log-likelihood, but want max likelihood
-		return np.argmin(lls) 
+		return np.argmin(lls)
 
 	def init_labels_from_gmm(self):
 		label_output = np.empty((self.image.height,self.image.width), dtype='int8')
@@ -78,9 +81,13 @@ class MRF():
 	def singleton(self, i, j, label):
 		# this is just the negative log-likelihood
 		# pdb.set_trace()
+		# print(i)
+		# print(self.image[i,j])
+		# print(self.means[label])
 		x_mu = np.matrix(self.image[i,j] - self.means[label])
+
 		return 0.5 * (math.log(self.var_dets[label]) + x_mu * self.var_invs[label] * x_mu.T)#.item(0) # unwrap numpy matrix
-	
+
 	def doubleton(self, i, j, label):
 		energy = 0.0
 
@@ -89,7 +96,7 @@ class MRF():
 					i + dy >= 0,
 					j + dx < self.image.width,
 					j + dx >= 0]):
-				if label == self.labels[i+dy,j+dx]: 
+				if label == self.labels[i+dy,j+dx]:
 					energy -= self.beta(i,j)
 				else:
 					energy += self.beta(i,j)
@@ -107,7 +114,7 @@ class MRF():
 				singletons += self.singleton(i,j,k)
 				doubletons += self.doubleton(i,j,k)
 
-		return singletons + doubletons/2
+		return singletons + doubletons#/2
 
 	def local_energy(self, i, j, label):
 		return self.singleton(i,j,label) + self.doubleton(i,j,label)
@@ -115,24 +122,58 @@ class MRF():
 	### estimation algos for updating labels
 	# efficient graph cut
 
-	def icm(self, thresh=0.05):
+
+	def update_params(self):
+
+		lab_points = [[] for _ in range(self.no_classes)]
+		for i in range(self.image.height):
+			for j in range(self.image.width):
+				for k in range(self.no_classes):
+					if self.labels[i,j] == k:
+						lab_points[k].append(self.image[i,j])
+
+		for k in range(self.no_classes):
+			lab_points[k] = np.array(lab_points[k])
+
+		# for k in range(self.no_classes):
+		# 	if len(lab_points[k])==0:
+		# 		del lab_points[k]
+		# 		self.no_classes -= 1
+
+		means = [(1/len(lab_points[k]))*lab_points[k].sum(axis=0) if len(lab_points[k])>0 else self.means[k] for k in range(self.no_classes)]
+		# diffs = [[np.matrix(lab_points[k][i]-means[k]) for i in range(len(lab_points[k]))] for k in range(self.no_classes)  ]
+		# variances = [np.matrix([[0.0,0,0],[0,0,0],[0,0,0]]) for _ in range(self.no_classes) ]
+		# for k in range(self.no_classes):
+		# 	if len(lab_points[k])>0:
+		# 		for i in range(len(lab_points[k])):
+		# 			variances[k]+=diffs[k][i]*diffs[k][i].T/len(lab_points[k])
+		# print(self.means[1])
+		self.means = means#, self.variances = means, variances
+
+
+
+
+	def icm(self, thresh=0.0000005):
 		# basically loop through everything picking minimizing labeling until "convergence"
+		print("icm")
 		E_old = self.global_energy()
 		delta_E = 999 # no do-while in python :v[
 		counter = 0
-
 		while delta_E > thresh and counter < 10: # threshold for convergence
 			delta_E = 0
 			# mix up the order the indices are visited
 			random.shuffle(self.indices)
 
 			for i, j in self.indices:
-				# local_energies = [self.local_energy(i,j,k) for k in range(self.no_classes)]
-				self.labels[i,j] = self.estimate_label(i,j)#np.argmin(local_energies)
+				local_energies = [self.local_energy(i,j,k) for k in range(self.no_classes)]
+				self.labels[i,j] = np.argmin(local_energies)#self.estimate_label(i,j)
 
 			energy = self.global_energy()
 			delta_E = math.fabs(E_old - energy)
 			E_old = energy
+			self.update_params()
+			print("THE COUNTER:", counter)
+			print(delta_E)
 			counter += 1
 
 		print('took {} iterations'.format(counter)) # \n final energy was {:6.6f}', E_old
@@ -158,38 +199,163 @@ class MRF():
 			energy = self.global_energy()
 			delta_E = math.fabs(E_old - energy)
 			E_old = energy
+			print("THE COUNTER:", counter)
 			counter += 1
 
 		print('took {} iterations\n final energy was {:6.6f}'.format(counter,E_old))
+
+
+class fisher_MRF(MRF):
+	def doubleton(self, i, j, label):
+		energy = 0.0
+		alpha = 1
+		beta = 1
+		for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
+			if all([i + dy < self.image.height,
+					i + dy >= 0,
+					j + dx < self.image.width,
+					j + dx >= 0]):
+				if label == self.labels[i+dy,j+dx]:
+					energy -= beta
+				else:
+					sig = np.matrix(self.variances[label] + self.variances[self.labels[i + dy, j + dx]])
+					fisher=(self.means[label] - self.means[self.labels[i+dy,j+dx]])*np.matrix(self.means[label] - self.means[self.labels[i+dy,j+dx]]).T#*np.linalg.inv(sig)*np.matrix(self.means[label] - self.means[self.labels[i+dy,j+dx]]).T
+					# print(fisher)
+					energy += alpha*fisher
+		return energy
+
+class hard_boundary_MRF(MRF):
+	def doubleton(self, i, j, label):
+		energy = 0.0
+		alpha = 100
+		beta = 1
+		for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+			if any([i + dy >= self.image.height,
+					i + dy < 0,
+					j + dx >= self.image.width,
+					j + dx < 0]):
+				return 0
+		count=0
+		negcount=0
+		# for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+		# 	if label == self.labels[i+dy,j+dx]:
+		# 		count+=1
+			# else:
+			# 	energy+=beta
+
+		# if count==4:
+		# 	energy-=count*beta
+		# else:
+		# 	energy+=count*beta
+
+		countH = 0
+		if label == self.labels[i-1,j] and label== self.labels[i+1,j]:
+			energy += alpha
+		if label == self.labels[i,j-1] and label==self.labels[i,j+1]:
+			energy += alpha
+		else:
+			energy-=alpha
+		return energy
+
+
+class fisher2_MRF(MRF):
+	def doubleton(self, i, j, label):
+		energy = 0.0
+		alpha = 10
+		beta = 1
+		for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
+			if all([i + dy < self.image.height,
+					i + dy >= 0,
+					j + dx < self.image.width,
+					j + dx >= 0]):
+				sig = np.matrix(self.variances[label] + self.variances[self.labels[i + dy, j + dx]])
+				fisher = (self.means[label] - self.means[self.labels[i + dy, j + dx]]) * np.matrix(self.means[label] - self.means[self.labels[i + dy, j + dx]]).T  # *np.linalg.inv(sig)*np.matrix(self.means[label] - self.means[self.labels[i+dy,j+dx]]).T
+				# print(fisher)
+				if label == self.labels[i+dy,j+dx]:
+					energy += -beta+alpha*(self.image[i,j]-self.image[i+dy,j+dx])*np.matrix(self.image[i,j]-self.image[i+dy,j+dx]).T
+				else:
+
+					energy += beta
+		return energy
+
+class pixel_MRF(MRF):
+	def doubleton(self, i, j, label):
+		energy = 0.0
+		thresh = 0.02
+		for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
+			if all([i + dy < self.image.height,
+					i + dy >= 0,
+					j + dx < self.image.width,
+					j + dx >= 0]):
+				if label == self.labels[i+dy,j+dx] and np.linalg.norm(self.image[i,j]-self.image[i+dy, j+dx])**2 < thresh:
+					energy -= self.beta(i,j)
+				else:
+					energy += self.beta(i,j)
+
+		return energy
+
+
+
+#
+#
+# class flaherty_MRF(MRF):
+# 	def doubleton(self, i, j):
+# 		energy = 0.0
+# 		for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+# 			if all([i + dy < self.image.height,
+# 					i + dy >= 0,
+# 					j + dx < self.image.width,
+# 					j + dx >= 0]):
+# 				if label == self.labels[i + dy, j + dx]:
+# 					energy -= self.beta(i, j)
+# 				else:
+# 					energy += self.beta(i, j)#NOT FINISHED
+# 		return energy
+
+
+
+
 
 # gmm with estimate_parameters function
 # -> mle to get initial segmentation
 
 if __name__ == '__main__':
 	import matplotlib.pyplot as plt
-	K = 5
+	K = 3
 	# test_img = Image() # should raise error
-	test_img = Image('./test_resized_2.jpg')
-	means = [np.array([random.uniform(0,1),random.uniform(0,1),random.uniform(0,1) ]) for _ in range(K)]
-	variances = [random.uniform(1,2) * np.eye(3)] * K
-	
+	test_img = Image('./pattern.png')#Image('./test_resized_2.jpg')
+
+	# means = [np.array([random.uniform(0,1),random.uniform(0,1),random.uniform(0,1) ]) for _ in range(K)]
+	# means = [np.array([1/k,1/k,1/k]) for k in range(1,K+1)]
+	variances = [ np.eye(3)] * K#random.uniform(1,2) *
+
 	# # #
 	 ### this is how i interface w/ ur code
 	# # #
 
-	# test_gmm = oliver_gmm(test_img._data,3)
-
+	test_gmm = oliver_gmm(test_img,K)
+	# plt.imshow(test_img._data)
+	# plt.show()
 	# init_pi = [0.33, 0.33, 0.34]
 	# init_mu = [np.array([0.5, 0.5, 0.5])]*3
 	# init_sigma = [np.matrix([[1000.0, 0.0, 0.0], [0.0, 157.0, 0.0], [0.0, 0.0, 1.0]]) for _ in range(3)]
 
-	# test_gmm.estimate_parameters(10) 
+	[pi_est, means, not_variances] = test_gmm.estimate_parameters(25)
 
-
-	test_mrf = MRF(test_img,means,variances)
-	plt.imshow(test_mrf.labels,cmap='gist_gray_r')
-	plt.savefig('before.png')
+	test_mrf = fisher2_MRF(test_img,means,variances)
+	# test_mrf.doubleton = new_doubleton
+	plt.imshow(test_mrf.labels)
+	# plt.show()
+	plt.savefig('before.png',cmap='gist_gray_r')
 	test_mrf.icm()
+	# test_mrf.gibbs()
 	plt.imshow(test_mrf.labels)
 	plt.savefig('after_icm.png',cmap='gist_gray_r')
+	im = np.zeros([test_img.height,test_img.width,3])
+	for i in range(test_img.height):
+		for j in range(test_img.width):
+			im[i,j] = test_mrf.means[test_mrf.labels[i,j]]
+	plt.imshow(im)
+	plt.savefig('real_means.png',cmap='gist_gray_r')
+	# plt.savefig('after_gibbs.png',cmap='gist_gray_r')
 	# lol, for my 640 x 425 px image this code took 140 seconds to run on my desktop